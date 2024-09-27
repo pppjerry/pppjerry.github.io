f57dee4f9ce97bb7e3d1d645b154b1bea3082d39
pangjerry <310338018@qq.com>
2024-09-27 08:55:47 +0800

    Site updated: 2024-09-27 08:55:46

--- js/main.js
+++ js/main.js
@@ -1,210 +1,113 @@
-// utils
-const util = {
-
-  // https://github.com/jerryc127/hexo-theme-butterfly
-  diffDate: (d, more = false) => {
-    const dateNow = new Date()
-    const datePost = new Date(d)
-    const dateDiff = dateNow.getTime() - datePost.getTime()
-    const minute = 1000 * 60
-    const hour = minute * 60
-    const day = hour * 24
-
-    let result
-    if (more) {
-      const dayCount = dateDiff / day
-      const hourCount = dateDiff / hour
-      const minuteCount = dateDiff / minute
-
-      if (dayCount > 14) {
-        result = null
-      } else if (dayCount >= 1) {
-        result = parseInt(dayCount) + ' ' + ctx.date_suffix.day
-      } else if (hourCount >= 1) {
-        result = parseInt(hourCount) + ' ' + ctx.date_suffix.hour
-      } else if (minuteCount >= 1) {
-        result = parseInt(minuteCount) + ' ' + ctx.date_suffix.min
-      } else {
-        result = ctx.date_suffix.just
-      }
-    } else {
-      result = parseInt(dateDiff / day)
-    }
-    return result
-  },
-
-  copy: (id, msg) => {
-    const el = document.getElementById(id);
-    if (el) {
-      el.select();
-      document.execCommand("Copy");
-      if (msg && msg.length > 0) {
-        hud.toast(msg, 2500);
-      }
-    }
-  },
-
-  toggle: (id) => {
-    const el = document.getElementById(id);
-    if (el) {
-      el.classList.toggle("display");
-    }
-  },
-
-  scrollTop: () => {
-    window.scrollTo({top: 0, behavior: "smooth"});
-  },
-
-  scrollComment: () => {
-    document.getElementById('comments').scrollIntoView({behavior: "smooth"});
-  },
-
-  viewportLazyload: (target, func, enabled = true) => {
-    if (!enabled || !("IntersectionObserver" in window)) {
-      func();
-      return;
-    }
-    const observer = new IntersectionObserver((entries) => {
-      if (entries[0].intersectionRatio > 0) {
-        func();
-        observer.disconnect();
-      }
-    });
-    observer.observe(target);
-  }
+/**
+ * Sets up Justified Gallery.
+ */
+if (!!$.prototype.justifiedGallery) {
+  var options = {
+    rowHeight: 140,
+    margins: 4,
+    lastRow: "justify"
+  };
+  $(".article-gallery").justifiedGallery(options);
 }
 
-const hud = {
-  toast: (msg, duration) => {
-    const d = Number(isNaN(duration) ? 2000 : duration);
-    var el = document.createElement('div');
-    el.classList.add('toast');
-    el.classList.add('show');
-    el.innerHTML = msg;
-    document.body.appendChild(el);
+$(document).ready(function() {
 
-    setTimeout(function(){ document.body.removeChild(el) }, d);
-    
-  },
-
-}
-
-// defines
-
-const l_body = document.querySelector('.l_body');
-
-
-const init = {
-  toc: () => {
-    utils.jq(() => {
-      const scrollOffset = 32;
-      var segs = [];
-      $("article.md-text :header").each(function (idx, node) {
-        segs.push(node);
-      });
-      function activeTOC() {
-        var scrollTop = $(this).scrollTop();
-        var topSeg = null;
-        for (var idx in segs) {
-          var seg = $(segs[idx]);
-          if (seg.offset().top > scrollTop + scrollOffset) {
-            continue;
-          }
-          if (!topSeg) {
-            topSeg = seg;
-          } else if (seg.offset().top >= topSeg.offset().top) {
-            topSeg = seg;
-          }
-        }
-        if (topSeg) {
-          $("#data-toc a.toc-link").removeClass("active");
-          var link = "#" + topSeg.attr("id");
-          if (link != '#undefined') {
-            const highlightItem = $('#data-toc a.toc-link[href="' + encodeURI(link) + '"]');
-            if (highlightItem.length > 0) {
-              highlightItem.addClass("active");
-            }
-          } else {
-            $('#data-toc a.toc-link:first').addClass("active");
-          }
-        }
-      }
-      function scrollTOC() {
-        const e0 = document.querySelector('#data-toc .toc');
-        const e1 = document.querySelector('#data-toc .toc a.toc-link.active');
-        if (e0 == null || e1 == null) {
-          return;
-        }
-        const offsetBottom = e1.getBoundingClientRect().bottom - e0.getBoundingClientRect().bottom + 100;
-        const offsetTop = e1.getBoundingClientRect().top - e0.getBoundingClientRect().top - 64;
-        if (offsetTop < 0) {
-          e0.scrollBy({top: offsetTop, behavior: "smooth"});
-        } else if (offsetBottom > 0) {
-          e0.scrollBy({top: offsetBottom, behavior: "smooth"});
-        }
-      }
-      
-      var timeout = null;
-      window.addEventListener('scroll', function() {
-        activeTOC();
-        if(timeout !== null) clearTimeout(timeout);
-        timeout = setTimeout(function() {
-          scrollTOC();
-        }.bind(this), 50);
-      });      
-    })
-  },
-  sidebar: () => {
-    utils.jq(() => {
-      $("#data-toc a.toc-link").click(function (e) {
-        sidebar.dismiss();
-      });
-    })
-  },
-  relativeDate: (selector) => {
-    selector.forEach(item => {
-      const $this = item
-      const timeVal = $this.getAttribute('datetime')
-      let relativeValue = util.diffDate(timeVal, true)
-      if (relativeValue) {
-        $this.innerText = relativeValue
-      }
-    })
-  },
   /**
-   * Tabs tag listener (without twitter bootstrap).
+   * Shows the responsive navigation menu on mobile.
    */
-  registerTabsTag: function () {
-    // Binding `nav-tabs` & `tab-content` by real time permalink changing.
-    document.querySelectorAll('.tabs .nav-tabs .tab').forEach(element => {
-      element.addEventListener('click', event => {
-        event.preventDefault();
-        // Prevent selected tab to select again.
-        if (element.classList.contains('active')) return;
-        // Add & Remove active class on `nav-tabs` & `tab-content`.
-        [...element.parentNode.children].forEach(target => {
-          target.classList.toggle('active', target === element);
-        });
-        // https://stackoverflow.com/questions/20306204/using-queryselector-with-ids-that-are-numbers
-        const tActive = document.getElementById(element.querySelector('a').getAttribute('href').replace('#', ''));
-        [...tActive.parentNode.children].forEach(target => {
-          target.classList.toggle('active', target === tActive);
-        });
-        // Trigger event
-        tActive.dispatchEvent(new Event('tabs:click', {
-          bubbles: true
-        }));
-      });
+  $("#header > #nav > ul > .icon").click(function() {
+    $("#header > #nav > ul").toggleClass("responsive");
+  });
+
+
+  /**
+   * Controls the different versions of  the menu in blog post articles 
+   * for Desktop, tablet and mobile.
+   */
+  if ($(".post").length) {
+    var menu = $("#menu");
+    var nav = $("#menu > #nav");
+    var menuIcon = $("#menu-icon, #menu-icon-tablet");
+
+    /**
+     * Display the menu on hi-res laptops and desktops.
+     */
+    if ($(document).width() >= 1440) {
+      menu.show();
+      menuIcon.addClass("active");
+    }
+
+    /**
+     * Display the menu if the menu icon is clicked.
+     */
+    menuIcon.click(function() {
+      if (menu.is(":hidden")) {
+        menu.show();
+        menuIcon.addClass("active");
+      } else {
+        menu.hide();
+        menuIcon.removeClass("active");
+      }
+      return false;
     });
 
-    window.dispatchEvent(new Event('tabs:register'));
-  },
+    /**
+     * Add a scroll listener to the menu to hide/show the navigation links.
+     */
+    if (menu.length) {
+      $(window).on("scroll", function() {
+        var topDistance = menu.offset().top;
 
-}
+        // hide only the navigation links on desktop
+        if (!nav.is(":visible") && topDistance < 50) {
+          nav.show();
+        } else if (nav.is(":visible") && topDistance > 100) {
+          nav.hide();
+        }
 
+        // on tablet, hide the navigation icon as well and show a "scroll to top
+        // icon" instead
+        if ( ! $( "#menu-icon" ).is(":visible") && topDistance < 50 ) {
+          $("#menu-icon-tablet").show();
+          $("#top-icon-tablet").hide();
+        } else if (! $( "#menu-icon" ).is(":visible") && topDistance > 100) {
+          $("#menu-icon-tablet").hide();
+          $("#top-icon-tablet").show();
+        }
+      });
+    }
 
-// init
-init.toc()
-init.sidebar()
-init.relativeDate(document.querySelectorAll('#post-meta time'))
-init.registerTabsTag()
+    /**
+     * Show mobile navigation menu after scrolling upwards,
+     * hide it again after scrolling downwards.
+     */
+    if ($( "#footer-post").length) {
+      var lastScrollTop = 0;
+      $(window).on("scroll", function() {
+        var topDistance = $(window).scrollTop();
+
+        if (topDistance > lastScrollTop){
+          // downscroll -> show menu
+          $("#footer-post").hide();
+        } else {
+          // upscroll -> hide menu
+          $("#footer-post").show();
+        }
+        lastScrollTop = topDistance;
+
+        // close all submenu"s on scroll
+        $("#nav-footer").hide();
+        $("#toc-footer").hide();
+        $("#share-footer").hide();
+
+        // show a "navigation" icon when close to the top of the page, 
+        // otherwise show a "scroll to the top" icon
+        if (topDistance < 50) {
+          $("#actions-footer > #top").hide();
+        } else if (topDistance > 100) {
+          $("#actions-footer > #top").show();
+        }
+      });
+    }
+  }
+});
